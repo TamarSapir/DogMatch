cd443352d3d4f2b69206b730f56d38d3e79cfdd7
Tamar Sapir <Tamarsapir05@gmail.com>
2025-12-06 22:45:04 +0200

    Connect frontend to new backend URL

--- dog-match/src/App.vue
+++ dog-match/src/App.vue
@@ -22,7 +22,7 @@ type Dog = {
   imageUrl: string;
 };
 
-const API_URL = "https://dogmatch-fso5.onrender.com";
+const API_URL = "https://dogmatch-api.onrender.com";
 
 const MOCK_DOGS: Dog[] = [
   {
